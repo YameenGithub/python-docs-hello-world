4bee846b134273c3774d0dd5ac645997f0dab6ea
YameenGithub <130193248+YameenGithub@users.noreply.github.com>
2024-08-31 15:50:49 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestHelloWorld(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual("hello".upper(), "HELLO")
-
-if __name__ == "__main__":
-    unittest.main()
