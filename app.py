08cf9fd7fe8f7d370f30fb1299e8103b192b15b7
YameenGithub <130193248+YameenGithub@users.noreply.github.com>
2024-08-31 17:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Me!"
+    return (
+        "Hello, World from Me!<br>"
+        "How are you? Hope you are doing great!<br>"
+        "Have a wonderful day ahead!"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
